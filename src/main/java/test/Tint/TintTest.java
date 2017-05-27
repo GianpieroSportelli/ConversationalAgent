47f89dc774918b0eaa51bfe61172a0449b8c128c
gianpiero sportelli <SPORT@DESKTOP-SH0N04K>
2017-05-27 18:22:46 +0200

    Allineamento

--- src/main/java/test/Tint/TintTest.java
+++ src/main/java/test/Tint/TintTest.java
@@ -5,14 +5,15 @@
  */
 package test.Tint;
 
+import configuration.Config;
 import edu.stanford.nlp.pipeline.Annotation;
 import eu.fbk.dh.tint.runner.TintPipeline;
 import eu.fbk.dh.tint.runner.TintRunner;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import org.json.JSONObject;
 
@@ -23,14 +24,16 @@ import org.json.JSONObject;
 public class TintTest {
 
     public static void main(String[] args) throws IOException {
-        String input = "ho speso 5 euro?";
+        String input = "alle 4 di domaini mattina, ora, tra 1 minuto, per 2 ore, mercoledi prossimo, questa settimana, giovedi scorso, la prossima settimana, il prossimo mese ho speso 200 euro";
         String output="";
         TintPipeline pipeline = new TintPipeline();
-        pipeline.loadDefaultProperties();
+        //pipeline.loadDefaultProperties();
+        System.out.println(Config.PATH_TINTCONF);
+        pipeline.loadPropertiesFromFile(new File(Config.PATH_TINTCONF));
         pipeline.load();
         InputStream stream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
         ByteArrayOutputStream out=new ByteArrayOutputStream();
-        Annotation annotation=pipeline.run(stream, out, TintRunner.OutputFormat.JSON);
+        pipeline.run(stream, out, TintRunner.OutputFormat.JSON);
         
         output=new String(out.toByteArray());
         JSONObject json=new JSONObject(output);
